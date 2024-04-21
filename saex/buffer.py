Fix activation masking

saex/buffer.py
@@ -25,21 +25,17 @@ class ActivationBuffer(eqx.Module):
         self._index = eqx.nn.StateIndex(jax.device_put(jnp.array(0)))
 
     @partial(eqx.filter_jit, donate="all-except-first")
-    def __call__(self, activations, state, mask=None):
+    def __call__(self, activations, state):
         cache, n_valid, index = state.get(self._cache), state.get(self._n_valid), state.get(self._index)
-        if mask is None:
-            mask = jnp.ones(len(activations), dtype=jnp.bool)
-        offsets = jnp.cumsum(mask.astype(jnp.int32)) - 1
-        new_n_valid = jnp.minimum(n_valid + offsets[-1], self.max_samples)
+        offsets = jnp.arange(activations.shape[0])
+        new_n_valid = jnp.minimum(n_valid + activations.shape[0], self.max_samples)
         # if n_valid == max_samples, we want to overwrite the oldest samples
         indices = (index + offsets) % self.max_samples
-        new_index = (index + offsets[-1]) % self.max_samples
+        new_index = (index + activations.shape[0]) % self.max_samples
         return (state
                 .set(self._cache,
                      cache
-                     # TODO properly order indices so one .set() does the job
-                     .at[indices].set(0)
-                     .at[indices].add(activations.astype(cache.dtype) * mask[:, None]))
+                     .at[indices].set(activations.astype(cache.dtype)))
                 .set(self._n_valid, new_n_valid)
                 .set(self._index, new_index))
 

saex/trainer_cache.py
@@ -177,19 +177,33 @@ class BufferTrainer(object):
             return sae_params, sae_state, opt_state, stats
 
         bar = trange(self.config.train_iterations + self.config.dry_run_steps)
+        tokens_processed = 0
         try:
             for iteration in bar:
                 if (iteration % self.config.cache_every_steps == 0
                     or iteration < self.config.dry_run_steps
                     or self.buffer is None):
-                    for _ in range(self.config.cache_acc if self.buffer is not None else 1):
+                    mid_buffer = jnp.empty((
+                        self.config.cache_acc * self.config.cache_batch_size * self.config.model_config.max_seq_len,
+                        self.config.n_dimensions), dtype=self.config.buffer_dtype)
+                    accumulated = 0
+                    while accumulated < mid_buffer.shape[0]:
                         # cache more activations
                         texts = []
                         for _ in range(self.config.cache_batch_size):
                             texts.append(next(dataset_iterator))
                         activations, model_misc = self.model(texts)
-                        if self.buffer is not None and iteration == 0:
-                            self.buffer_state = self.buffer(activations, self.buffer_state, mask=model_misc.get("mask"))
+                        mask = model_misc.get("mask")
+                        if mask is not None:
+                            n_tokens = mask.sum()
+                            raw_tokens = activations.reshape(-1, activations.shape[-1])[mask.flatten()]
+                            mid_buffer = mid_buffer.at[accumulated:accumulated + n_tokens].set(raw_tokens)  # ..
+                            accumulated += n_tokens
+                        else:
+                            n_tokens = np.prod(activations.shape[:-1])
+                        tokens_processed += n_tokens
+                    if self.buffer is not None:
+                        self.buffer_state = self.buffer(mid_buffer, self.buffer_state)
                 
                 if iteration < self.config.dry_run_steps:
                     bar.set_description("Caching activations")
@@ -198,7 +212,7 @@ class BufferTrainer(object):
 
                 # train SAE
                 if self.buffer is None:
-                    batch = activations
+                    batch = mid_buffer
                 else:
                     key, subkey = jax.random.split(key, 2)
                     # self.buffer_state, batch = self.buffer.sample_batch(
@@ -216,15 +230,16 @@ class BufferTrainer(object):
                 sae_params, self.sae_state, opt_state, stats = train_step(
                     batch, sae_params, self.sae_state, opt_state, sae_static, optimizer,
                     iteration, subkey)
+                stats["tokens_processed"] = tokens_processed
 
                 bar.set_postfix(stats)
                 
                 if self.config.use_wandb:
                     if iteration % self.config.log_every == 0:
-                        run.log(stats)
+                        run.log(stats, step=iteration)
                     if iteration % self.config.hist_every == self.config.hist_every - 1:
                         # look at this graph
-                        run.log({"histogram": wandb.Histogram(self.sae.get_log_sparsity(self.sae_state))})
+                        run.log({"histogram": wandb.Histogram(self.sae.get_log_sparsity(self.sae_state))}, step=iteration)
                 
                 # TODO: track in wandb or other logger:
                 # - learning rate